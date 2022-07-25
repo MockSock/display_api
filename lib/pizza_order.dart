f202ec601af9b1f643c84cb175f512d0228bcf6b
madden20202 <business.mocksock@gmail.com>
2022-07-24 21:12:46 -0500

    create and initialize pizzaOrder

--- lib/pizza_order.dart
+++ lib/pizza_order.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 import './pizza_model.dart';
 
 class PizzaOrder extends StatelessWidget {
+  final Pizza pizzaOrder;
+
+  PizzaOrder(this.pizzaOrder);
+
   @override
   Widget build(BuildContext context) {
     return Container();
