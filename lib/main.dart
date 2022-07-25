7ab6b8c8aeb5c37a18df034852a4abddb8d1fbbe
madden20202 <business.mocksock@gmail.com>
2022-07-24 21:06:49 -0500

    get ready for next portion

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var _pizzaOrders = const [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
